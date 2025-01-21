a7e1f6d3806ec616f60698af921784358f0d23e3
logistow <228302287+logistow@users.noreply.github.com>
2025-01-21 20:17:47 +0100

    define resolvers and gql schema

---
+++ src/schema/resolver.ts
@@ -0,0 +1,49 @@
+import { UserEntity } from "database/entity";
+import { AppDataSource } from "database";
+
+const userRepository = AppDataSource.getRepository(UserEntity);
+
+const resolvers = {
+  Query: {
+    getUser: async (_, { id }: { id: number }) => {
+      return await userRepository.findOneBy({ id: id });
+    },
+
+    getUsers: async () => {
+      return await userRepository.find();
+    },
+  },
+  Mutation: {
+    createUser: async (
+      _,
+      { name, email, age }: { name: string; email: string; age: number }
+    ) => {
+      const user = userRepository.create({ name, email, age });
+      return await userRepository.save(user);
+    },
+
+    updateUser: async (
+      _,
+      {
+        id,
+        name,
+        email,
+        age,
+      }: { id: number; name?: string; email?: string; age?: number }
+    ) => {
+      await userRepository.update(id, { name, email, age });
+      return await userRepository.findOneBy({id: id});
+    },
+
+    deleteUser: async (_, { id }: { id: number }) => {
+      const user = await userRepository.findOneBy({id: id});
+      if (user) {
+        await userRepository.remove(user);
+        return user;
+      }
+      throw new Error("User not found");
+    },
+  },
+};
+
+export default resolvers;
